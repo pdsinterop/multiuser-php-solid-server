2620d170c3e5a83c8352d2680398ce53c936d13a
Yvo Brevoort <yvo@muze.nl>
2025-07-01 00:37:48 +0200

    empty version of notifications

---
+++ lib/SolidNotifications.php
@@ -0,0 +1,10 @@
+<?php
+    namespace Pdsinterop\PhpSolid;
+    
+    use Pdsinterop\Solid\SolidNotifications\SolidNotificationsInterface;
+
+    class SolidNotifications implements SolidNotificationsInterface
+    {
+        public function send($path, $type) {
+        }
+    }
