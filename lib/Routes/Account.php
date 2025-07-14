aee5981b2b0f5dcba8aed4eb5e00017c552ce6f6
Yvo Brevoort <yvo@muze.nl>
2025-07-14 14:11:19 +0200

    use ??

--- lib/Routes/Account.php
+++ lib/Routes/Account.php
@@ -243,8 +243,8 @@
 			$request = $requestFactory->fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
 			$requestBody = $request->getParsedBody();
 
-			$grantType = isset($requestBody['grant_type']) ? $requestBody['grant_type'] : null;
-			$clientId = isset($requestBody['client_id']) ? $requestBody['client_id'] : null;
+			$grantType = $requestBody['grant_type'] ?? null;
+			$clientId = $requestBody['client_id'] ?? null;
 			switch ($grantType) {
 				case "authorization_code":
 					$code = $requestBody['code'];
