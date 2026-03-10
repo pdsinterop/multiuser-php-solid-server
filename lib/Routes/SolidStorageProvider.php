2aa78af79684e5f1cef26edd83a41cbbe294498f
Yvo Brevoort <yvo@muze.nl>
2026-03-10 11:54:09 +0100

    add routes

---
+++ lib/Routes/SolidStorageProvider.php
@@ -0,0 +1,44 @@
+<?php
+	namespace Pdsinterop\PhpSolid\Routes;
+
+	use Pdsinterop\PhpSolid\StorageServer;
+
+	class SolidStorageProvider {
+		public static function respondToStorageNew() {
+			try {
+				$webId = StorageServer::getWebId($rawRequest);
+
+				if (!isset($webId)) {
+					$response = $resourceServer->getResponse()
+						->withStatus(409, "Invalid token");
+					StorageServer::respond($response);
+					exit();
+				}
+			} catch (\Throwable $e) {
+				$webId = $_POST['webId'];
+				// FIXME: Check against a trusted remote party;
+			}
+
+			if (!isset($webId)) {
+				header("HTTP/1.1 400 Bad Request");
+				exit();
+			}
+
+			$createdStorage = StorageServer::createStorage($webId);
+			if (!$createdStorage) {
+				error_log("Failed to create storage");
+				header("HTTP/1.1 400 Bad Request");
+				exit();
+			}
+
+			//Mailer::sendStorageCreated($createdStoage);
+
+			$responseData = array(
+				"storageId" => $createdStorage['storageId']
+			);
+			header("HTTP/1.1 201 Created");
+			header("Content-type: application/json");
+			echo json_encode($responseData, JSON_PRETTY_PRINT | JSON_THROW_ON_ERROR);
+		}
+	}
+			
