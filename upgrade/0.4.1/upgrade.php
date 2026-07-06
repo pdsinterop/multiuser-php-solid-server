7c4f77ffc78e22cda6ea28aa6ea0dec36841b70b
Yvo Brevoort <yvo@muze.nl>
2026-07-06 11:58:32 +0200

    whitespace

--- upgrade/0.4.1/upgrade.php
+++ upgrade/0.4.1/upgrade.php
@@ -6,25 +6,25 @@ use Pdsinterop\PhpSolid\Server;
 
 function upgradeDatabase()
 {
-    $statements = [
-        'BEGIN TRANSACTION',
-        'CREATE TABLE allowedClients_new (userId VARCHAR(255) NOT NULL, clientId VARCHAR(255) NOT NULL)',
-        'INSERT INTO allowedClients_new (userId, clientId) SELECT userId, clientId FROM allowedClients',
-        'DROP TABLE allowedClients',
-        'ALTER TABLE allowedClients_new RENAME TO allowedClients',
-        'COMMIT'
-    ];
+	$statements = [
+		'BEGIN TRANSACTION',
+		'CREATE TABLE allowedClients_new (userId VARCHAR(255) NOT NULL, clientId VARCHAR(255) NOT NULL)',
+		'INSERT INTO allowedClients_new (userId, clientId) SELECT userId, clientId FROM allowedClients',
+		'DROP TABLE allowedClients',
+		'ALTER TABLE allowedClients_new RENAME TO allowedClients',
+		'COMMIT'
+	];
 
-    try {
-        $pdo = new \PDO("sqlite:" . DBPATH);
+	try {
+		$pdo = new \PDO("sqlite:" . DBPATH);
 
-        // create tables
-        foreach($statements as $statement){
-            $pdo->exec($statement);
-        }
-    } catch(\PDOException $e) {
-        echo $e->getMessage();
-    }
+		// create tables
+		foreach($statements as $statement){
+			$pdo->exec($statement);
+		}
+	} catch(\PDOException $e) {
+		echo $e->getMessage();
+	}
 }
 
 upgradeDatabase();
