d398a9ac3bcf61c71d8729510c9c3802bf1b91fe
Yvo Brevoort <yvo@muze.nl>
2026-07-06 11:51:53 +0200

    add upgrade script to fix the db

---
+++ upgrade/0.4.1/upgrade.php
@@ -0,0 +1,30 @@
+<?php
+require_once(__DIR__ . "/../../config.php");
+require_once(__DIR__ . "/../../vendor/autoload.php");
+
+use Pdsinterop\PhpSolid\Server;
+
+function upgradeDatabase()
+{
+    $statements = [
+        'BEGIN TRANSACTION',
+        'CREATE TABLE allowedClients_new (userId VARCHAR(255) NOT NULL, clientId VARCHAR(255) NOT NULL)',
+        'INSERT INTO allowedClients_new (userId, clientId) SELECT userId, clientId FROM allowedClients',
+        'DROP TABLE allowedClients',
+        'ALTER TABLE allowedClients_new RENAME TO allowedClients',
+        'COMMIT'
+    ];
+
+    try {
+        $pdo = new \PDO("sqlite:" . DBPATH);
+
+        // create tables
+        foreach($statements as $statement){
+            $pdo->exec($statement);
+        }
+    } catch(\PDOException $e) {
+        echo $e->getMessage();
+    }
+}
+
+upgradeDatabase();
